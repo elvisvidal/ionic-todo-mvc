f8c66a6b73d02ea232940f8122cbb6ae110355cc
Elvis Vidal <elvis.vidal@3mw.com.br>
2016-10-08 19:55:33 -0300

    Inserindo tarefas a lista

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,34 @@
 angular.module('starter.controllers', [])
 
 // =================== ALL
-.controller('AllCtrl', function($scope) {
-  $scope.newTask = {
-    title:'OLA MUNDO'
-  };
+.controller('AllCtrl', function($scope, localStorage) {
+  var todos = $scope.todos = localStorage.todos;
+
+  $scope.newTask = {title: ''};
 
   $scope.addTask = function() {
-    console.log($scope.newTask);
+    var newTask = {
+      title: $scope.newTask.title.trim(),
+      completed: false
+    };
+
+    if (!newTask.title) {
+      return;
+    }
+
+    $scope.saving = true;
+    localStorage.insert(newTask)
+                .then(function success() {
+                  $scope.newTask = {title: ''};
+                })
+                .finally(function () {
+                  $scope.saving = false;
+                });
   };
 })
 
 // =================== ACTIVE
-.controller('ActiveCtrl', function($scope, Chats) {})
+.controller('ActiveCtrl', function($scope) {})
 
 // =================== COMPLETED
 .controller('CompletedCtrl', function($scope) {
